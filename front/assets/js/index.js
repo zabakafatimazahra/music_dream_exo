cc4e77a2bb893fc7650c904f7801164721c5c099
Erwin <erwin.maitret@gmail.com>
2022-10-12 16:48:13 +0200

    modifs taille guitare

--- front/assets/js/index.js
+++ front/assets/js/index.js
@@ -21,13 +21,14 @@ fetch("../back/guitars.json")
     let placeholder = document.querySelector("#listeGuitares");
     let out = "";
     for(let guitares of listeGuitares) {
-      out += `<div>
-      <img src="../back/images/${guitares.imageUrl}"/>
-      <h3> ${guitares.name}</h3>
-      <p> ${guitares.price} ou ${guitares.monthly}/mois</p>
-      <p> ${guitares.stars}</p>
-      </div>
-      `;
+      out += `
+              <div>
+                <img src="../back/images/${guitares.imageUrl}"/>
+                <h3> ${guitares.name}</h3>
+                <p> ${guitares.price} ou ${guitares.monthly}/mois</p>
+                <p> ${guitares.stars}</p>
+              </div>
+            `;
       }
     placeholder.innerHTML = out;
   })
@@ -37,46 +38,54 @@ fetch("../back/guitars.json")
     console.error(error);
   });
 
-
-
-
-
-  //Fetch pour récupérer "les plus populaires"
-  fetch("../back/populars.json")
-  .then(function (res) {
-    if (res.ok) {
-      return res.json();
-    }
-  })
-  .then(function(listePopulaires) {
-    let placeholder = document.querySelector("#listePopulaires");
-    let out = "";
-    for(let populaire of listePopulaires) {
-      out += `
+//Fetch pour récupérer "les plus populaires"
+fetch("../back/populars.json")
+.then(function (res) {
+  if (res.ok) {
+    return res.json();
+  }
+})
+.then(function(listePopulaires) {
+  let placeholder = document.querySelector("#listePopulaires");
+  let out = "";
+  for(let populaire of listePopulaires) {
+    out += `
+            <div>
               <img src="../back/images/${populaire.imageUrl}">
               <h3> ${populaire.name}</h3>
               <p> ${populaire.price} ou ${populaire.monthly}/mois</p>
               <p> ${populaire.stars}</p>
-            `;
-    }
-    placeholder.innerHTML = out;
-  })
-  .catch(function(error) {
-    console.error("Something goes wrong!");
-    console.error(error);
-  });
+            </div>
+          `;
+  }
+  placeholder.innerHTML = out;
+})
+.catch(function(error) {
+  console.error("Something goes wrong!");
+  console.error(error);
+});
 
-  //Fetch pour récupérer le "matériel studio"
-  fetch("../back/studio.json")
-  .then(function (res) {
-    if (res.ok) {
-      return res.json();
-    }
-  })
-  .then(data => {
-    console.table(data);
-  })
-  .catch(function(error) {
-    console.error("Something goes wrong!");
-    console.error(error);
-  });
+//Fetch pour récupérer le "matériel studio"
+fetch("../back/studio.json")
+.then(function (res) {
+  if (res.ok) {
+    return res.json();
+  }
+})
+.then(function(listeStudio) {
+  let placeholder = document.querySelector("#listeStudio");
+  let out = "";
+  for(let studio of listeStudio) {
+    out += `
+            <div>
+              <img src="../back/images/${studio.imageUrl}">
+              <h3> ${studio.title}</h3>
+            </div>
+          `;
+  }
+  placeholder.innerHTML = out;
+})
+.catch(function(error) {
+  console.error("Something goes wrong!");
+  console.error(error);
+});
